1c3e3684ec1bc0d6774d13d02b7be37d3b7caa50
seveibar <seveibar@gmail.com>
2024-08-09 16:22:07 -0700

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -5,6 +5,7 @@ import { hideBin } from "yargs/helpers"
 import * as fs from "fs/promises"
 import mkdirp from "mkdirp"
 import path from "path"
+import gitignore from "../package-lock.json" with { type: "buffer" }
 
 const argv = yargs(hideBin(process.argv))
   .option("dir", {
